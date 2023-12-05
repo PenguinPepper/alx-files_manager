68b9757eb30773774d9053365482823c11652bac
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-12-05 13:11:21 -0800

    Add logic to db.js

--- utils/db.js
+++ utils/db.js
@@ -2,22 +2,49 @@ import { MongoClient } from 'mongodb';
 
 class DBSClient {
   constructor() {
-    if (process.env.DB_HOST) {
-      this.host = process.env.DB_HOST;
-    } else {
-      this.host = 'localhost';
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.url = 'mongodb://' + this.host + ':' + this.port + '/' + this.database;
+    this.client = new MongoClient(this.url);
+  }
+
+  async init() {
+    try {
+      await this.client.connect();
+    } catch (err){
+      console.error('There is a problem. This is the problem: ', err);
+      throw err;
     }
-    if (process.env.DB_PORT) {
-      this.port = process.env.DB_PORT;
-    } else {
-      this.port = 27017;
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const db = this.client.db(this.database);
+    const users = db.collection('users');
+    try {
+      const usersNum = await users.countDocuments();
+      return usersNum;
+    } catch (err) {
+      return ('There is a problem. This is the problem: ', err);
     }
-    if (process.env.DB_DATABASE) {
-      this.database = process.env.DB_DATABASE;
-    } else {
-      this.database = 'files_manager';
+  }
+
+  async nbFiles() {
+    const db = this.client.db(this.database);
+    const files = db.collection('files');
+    try {
+      const fileNum = await files.count();
+      return fileNum;
+    } catch (err) {
+      return ('There is a problem. This is the problem: ', err);
     }
-    this.url = this.database + '//' + this.host + ':' + this.port;
-    this.client = new MongoClient(url);
   }
 }
+
+const dbClient = new DBSClient();
+dbClient.init().catch((err) => console.error('It didnt connect because: ', err));
+export default dbClient;
