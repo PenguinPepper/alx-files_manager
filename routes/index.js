86dd6414f571adc61934eebfcae933197b957ad5
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-12-06 13:38:41 -0800

    Add new routes in routes/index.js and controllers/UsersController.js containing UsersController object

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import { Router } from 'express';
+import express from 'express';
 
 const router = Router();
 
+router.use(express.json());
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', AuthController.getMe);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/', routes);
-app.use(express.json());
 
 app.listen(port, () => {
   console.log('Server is running on port: ', port);
