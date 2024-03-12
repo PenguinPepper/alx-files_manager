af0961b9394ea4d1725f27c90d8900a6281852a6
Elizabeth Pfachi <testlolabtest@gmail.com>
2024-03-12 01:11:40 -0700

    Add new route that posts users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const routes = Router();
 
 routes.get('/status', AppController.getStatus);
 routes.get('/stats', AppController.getStats);
+routes.post('/uers', UsersController.postNew);
 
 export default routes;
